b3dbbdca9a4b05ccedb9744fe5c97b5c917d365b
Nicholas Humfrey <njh@aelius.com>
2021-01-23 00:33:44 +0000

    Track current and target blind position Using timer to guess where the position of the blind is

--- velux-blind-control.ino
+++ velux-blind-control.ino
@@ -6,7 +6,7 @@
 // D3: connected to the Stop button
 // D4: connected to the Open button (up)
 //
-// Connect to the Arduinos serial port at 9600N1
+// Connect to the Arduino's serial port at 9600N1
 //
 // Send a command followed by a newline character:
 //   close
@@ -14,9 +14,57 @@
 //   open
 //
 
+#include <EveryTimerB.h>
+
+enum {
+    stateStopped,
+    stateOpening,
+    stateClosing
+} currentState = stateStopped;
+
+#define FULLY_OPEN   (100)
+#define FULLY_CLOSED (0)
+
+#define PIN_CLOSE  (2)
+#define PIN_STOP   (3)
+#define PIN_OPEN   (4)
+
+
+int currentPosition = FULLY_OPEN;
+int targetPosition = FULLY_OPEN;
+bool reportState = true;
+
+
+void updatePostion() {
+    if (currentState == stateOpening) {
+        currentPosition += 10;
+        if (currentPosition > FULLY_OPEN) {
+            currentPosition = FULLY_OPEN;
+            currentState = stateStopped;
+        }
+          reportState = true;
+    } else if (currentState == stateClosing) {
+        currentPosition -= 10;
+        if (currentPosition < FULLY_CLOSED) {
+            currentPosition = FULLY_CLOSED;
+            currentState = stateStopped;
+        }
+          reportState = true;
+    }
+
+    if (currentState != stateStopped && currentPosition == targetPosition) {
+        // We have reached the target position
+        currentState = stateStopped;
+        reportState = true;
+    }
+}
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
+
+    TimerB2.initialize();
+    TimerB2.attachInterrupt(updatePostion);
+    TimerB2.setPeriod(1600000); // 16 seconds / 10 steps = 1600000 microseconds
 
     Serial.begin(9600);
     Serial.println("# velux-blind-control");
@@ -37,17 +85,39 @@ void pulsePin(int pin)
 
 void closeCommand()
 {
-    pulsePin(2);
+    targetPosition = FULLY_CLOSED;
+    currentState = stateClosing;
+    pulsePin(PIN_CLOSE);
 }
 
 void stopCommand()
 {
-    pulsePin(3);
+    targetPosition = currentPosition;
+    currentState = stateStopped;
+    pulsePin(PIN_STOP);
 }
 
 void openCommand()
 {
-    pulsePin(4);
+    targetPosition = FULLY_OPEN;
+    currentState = stateOpening;
+    pulsePin(PIN_OPEN);
+}
+
+boolean isInteger(String str) {
+    unsigned int stringLength = str.length();
+
+    if (stringLength == 0) {
+        return false;
+    }
+
+    for(unsigned int i = 0; i < stringLength; ++i) {
+        char chr = str.charAt(i);
+        if (!isDigit(chr)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 void loop() {
@@ -63,8 +133,44 @@ void loop() {
         } else if (command.equalsIgnoreCase("stop")) {
             stopCommand();
             Serial.println("ok");
+        } else if (isInteger(command)) {
+            // Keep things simple and only open or close (nothing in-between)
+            int i = command.toInt();
+            if (i < 50) {
+               closeCommand();
+            } else if (i >= 50) {
+               openCommand();
+            }
+            Serial.println("ok");
         } else {
             Serial.println("error");
         }
     };
+
+    // FIXME: report current position less frequently?
+    if (reportState == true) {
+        Serial.print("target-position: ");
+        Serial.println(targetPosition, DEC);
+        Serial.print("current-position: ");
+        Serial.println(currentPosition, DEC);
+
+        switch(currentState) {
+           case stateClosing:
+              Serial.println("state: closing");
+           break;
+
+           case stateOpening:
+              Serial.println("state: opening");
+           break;
+
+           case stateStopped:
+              Serial.println("state: stopped");
+           break;
+
+           default:
+              Serial.println("state: error");
+           break;
+        }
+        reportState = false;
+    }
 }
